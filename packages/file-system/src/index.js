377e4dd117203351b9a597fda1d0f11b487fc4c9
root <774525000@qq.com>
2022-07-14 13:30:38 +0800

    beanCon

--- packages/file-system/src/index.js
+++ packages/file-system/src/index.js
@@ -4,20 +4,49 @@ const txt = document.querySelector('#txt')
 
 
 readBtn.addEventListener('click', async () => {
-    const handlerList = await window.showOpenFilePicker()
+    const options = {
+        types: [
+            {
+                description: 'txt文件选取',
+                accept: {
+                    'txt/*': ['.txt']
+                }
+            },
+        ],
+        excludeAcceptAllOption: true,
+        multiple: true
+    }
+
+    const handlerList = await window.showOpenFilePicker(options)
 
     txt.textContent = ''
-
-    for await (const handler of handlerList) {
-        if(handler.kind !== 'file' || !handler.name.endsWith('.txt')) {
+    for (const handler of handlerList) {
+        if (handler.kind !== 'file' || !handler.name.endsWith('.txt')) {
             return
         }
         const file = await handler.getFile()
         const reader = new FileReader()
         reader.readAsText(file)
         reader.onload = function () {
-            txt.textContent += this.result.toString()
+            txt.textContent += `${this.result}\n`
         }
     }
 
+})
+
+
+saveBtn.addEventListener('click', async () => {
+    const options = {
+        types: [
+            {
+                accept: {
+                    'txt/*': ['.txt']
+                }
+            }
+        ]
+    }
+    const handle = await window.showSaveFilePicker(options)
+    const stream = await handle.createWritable()
+    await stream.write(txt.textContent)
+    stream.close()
 })
